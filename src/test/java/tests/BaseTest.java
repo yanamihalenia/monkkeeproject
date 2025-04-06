15e5baf8e89433f128ec1a24c57d93438f6018e5
yanamihalenia <yana.miga@gmail.com>
2025-04-06 20:52:23 +0300

    before/after method

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,20 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.RegisterPage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
 public class BaseTest {
     HomePage homePage;
@@ -16,10 +27,26 @@ public class BaseTest {
         registerPage = new RegisterPage();
     }
 
+    @BeforeMethod
     public void initTest(){
         ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-notifications");
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        options.setExperimentalOption("prefs", prefs);
+        WebDriver driver = new ChromeDriver(options);
+        setWebDriver(driver);
 
+        Configuration.browser = "chrome";
+        Configuration.timeout = 15000;
+        Configuration.headless = false;
+        Configuration.browserSize = "1024x768";
+        initPages();
+    }
+
+    @AfterMethod
+    public void endTest(){
+        getWebDriver().quit();
     }
 }
