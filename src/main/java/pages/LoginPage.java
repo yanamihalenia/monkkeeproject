f02a1bebf3d84761b6781d38b6e80421a8aa75a0
yanamihalenia <yana.miga@gmail.com>
2025-04-07 19:57:31 +0300

    login tests

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -8,6 +8,9 @@ public class LoginPage extends BasePage {
     private static final String PASSWORD_INPUT = "#password";
     private static final String LOGIN_BUTTON_XPATH = "//*[@type='submit']";
     private static final String REGISTER_LINK_XPATH = "(//*[@class='login__links']/a)[2]";
+    private static final String ERROR_MESSAGE_USER_XPATH = "//*[@id='login']/../div";
+    private static final String ERROR_MESSAGE_PASSWORD_XPATH = "//*[@for='password']/../div/div[@class='help-block ng-binding']";
+
 
     public EntriesPage login(String username, String password, String url){
         open(url);
@@ -25,5 +28,13 @@ public class LoginPage extends BasePage {
         openLoginPage(LOGIN_PAGE_URL);
         $x(REGISTER_LINK_XPATH).click();
         return new RegisterPage();
+    }
+
+    public String getUsernameErrorMessageText(){
+        return $x(ERROR_MESSAGE_USER_XPATH).getText();
+    }
+
+    public String getPasswordErrorMessageText(){
+        return $x(ERROR_MESSAGE_PASSWORD_XPATH).getText();
     }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,12 +1,26 @@
 package tests;
 
 import constants.IConstants;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest{
+    public static final String ERROR_TEXT_MESSAGE = "Mandatory field";
 
     @Test(description = "2. Login with valid data")
     public void successLoginTest(){
         loginPage.login(USER, PASSWORD, IConstants.LOGIN_PAGE_URL);
+    }
+
+    @Test(description = "3. Login with empty username")
+    public void loginWithEmptyUsernameTest(){
+        loginPage.login("", PASSWORD, IConstants.LOGIN_PAGE_URL);
+        Assert.assertEquals(loginPage.getUsernameErrorMessageText(), ERROR_TEXT_MESSAGE);
+    }
+
+    @Test(description = "4. Login with empty password")
+    public void loginWithEmptyPasswordTest(){
+        loginPage.login(USER, "", IConstants.LOGIN_PAGE_URL);
+        Assert.assertEquals(loginPage.getPasswordErrorMessageText(), ERROR_TEXT_MESSAGE);
     }
 }
